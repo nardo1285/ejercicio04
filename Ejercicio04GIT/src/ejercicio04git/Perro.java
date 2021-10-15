9992ce18b551deccb3c13e58a1f04a5f7d45de3f
DAndresPeralta <danteandresperalta@gmail.com>
2021-10-15 18:57:35 -0300

    Segundo commit

---
+++ Ejercicio04GIT/src/ejercicio04git/Perro.java
@@ -0,0 +1,11 @@
+package ejercicio04git;
+
+public class Perro {
+
+    private String nombre;
+    private String apodo;
+    private int edad;
+    private String raza;
+    private String color;
+
+}
