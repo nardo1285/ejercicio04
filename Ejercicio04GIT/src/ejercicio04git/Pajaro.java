167fae699ba6599c6ba6c9449868a22fcf76342c
Marcelo Pintos <marcelo14527@hotmail.com>
2021-10-18 19:50:50 -0300

    modificacion

---
+++ Ejercicio04GIT/src/ejercicio04git/Pajaro.java
@@ -0,0 +1,42 @@
+
+package ejercicio04git;
+public class Pajaro {
+    private String nombre;
+    private int edad;
+    private String color;
+
+    public Pajaro() {
+    }
+
+    public Pajaro(String nombre, int edad, String color) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.color = color;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+            
+            
+}
